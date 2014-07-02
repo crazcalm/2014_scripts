820e184e78b335e5be371475d98213d16014751a
Marcus Willock <crazcalm@gmail.com>
2014-07-02 10:38:32 -0400

    Setting up the file for coding

---
+++ reddit_test.py
@@ -0,0 +1,18 @@
+"""
+Build a reddit search web service. Your service should use the reddit api’s to perform the search and return the results as described below.
+ 
+Helpful urls 
+     - Reddit Api -  http://www.reddit.com/dev/api
+     - Example reddit search call -  http://www.reddit.com/r/subreddits/search.json?q=test
+Service requirements
+     - sort the results by author name
+     - return the results in any format you see fit. (JSON, XML, YAML, plain text,  etc…..)
+Constraints 
+     - You can use any libraries or frameworks you see fit
+     - You must use Python as your programming language
+"""
+
+"""
+Python 3.4
+"""
+
